a8f5245bdd31f965efe7fb9a03d423879d85ffae
michinoku-YoRHa <kokoneko.it@gmail.com>
2025-07-10 22:19:35 +0900

    commit-1

--- lib/s3_cloudfront-stack.ts
+++ lib/s3_cloudfront-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Values } from 'aws-cdk-lib/aws-cloudwatch';
 
 export class S3CloudfrontStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const timestamp = Date.now();
+    
+    const s3Bucket = new s3.Bucket(this, 'WebSiteBucket', {
+      bucketName: `test-s3-bucket-${timestamp}`,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false,
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3CloudfrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3url = new cdk.CfnOutput(this, 'S3URL', {
+      value: s3Bucket.bucketWebsiteUrl,
+      description: 'S3 URL'
+    });
   }
 }

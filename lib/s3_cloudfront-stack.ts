85989572a1c9542dff34894d4488202d197d1869
michinoku-YoRHa <kokoneko.it@gmail.com>
2025-07-14 22:40:15 +0900

    commit-8_サイトデザイン変更

--- lib/s3_cloudfront-stack.ts
+++ lib/s3_cloudfront-stack.ts
@@ -10,7 +10,6 @@ import {
   aws_certificatemanager as acm,
 } from 'aws-cdk-lib';
 
-// 外部から取得するパラメータを定義
 interface S3CloudfrontStackProps extends cdk.StackProps {
   domainName: string;
   certificateArn: string;
@@ -29,7 +28,6 @@ export class S3CloudfrontStack extends cdk.Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
-    // ACM証明書の読み込み(別スタックから)
     const certificate = acm.Certificate.fromCertificateArn(this, 'WebSiteCert', props.certificateArn);
 
     const oac = new cloudfront.S3OriginAccessControl(this, 'OAC', {
@@ -46,19 +44,29 @@ export class S3CloudfrontStack extends cdk.Stack {
         ),          
       },
       defaultRootObject: 'index.html',
-        // 独自ドメインの指定
+        // エラー時にエラー用ページを表示
+      errorResponses: [
+        {
+            // エラー表示を行うステータスを指定
+          httpStatus: 404,
+            // 表示するエラー用ページを指定
+          responsePagePath: '/error.html',
+            // TTLの指定(0にするとデバック時に便利)
+          ttl: cdk.Duration.seconds(0),
+        },
+        {
+          httpStatus: 403,
+          responsePagePath: '/error.html',
+          ttl: cdk.Duration.seconds(0),
+        },
+      ],
       domainNames: [props.domainName],
-        // 証明書の指定
       certificate: certificate,
     });
 
-    // Route53レコード作成
     new route53.ARecord(this, 'AliasRecord', {
-        // ホストゾーン指定
       zone: props.hostedZone,
-        // レコード指定
       recordName: '',
-        // ターゲット指定
       target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(distribution)),
     });
 
